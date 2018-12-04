3b1c08dd0f3ed78820984a5a94d48106aa40f851
Eric Tobin <eatobin@gmail.com>
2018-12-04 13:40:55 -0600

    great lesson

--- eat_owership/src/main.rs
+++ eat_owership/src/main.rs
@@ -1,26 +1,68 @@
 #![allow(unused)]
+
+fn val_and_ownership_not_returned(s: String) -> () {
+    println!("{}", s);
+}
+
+fn val_and_ownership_returned(s: String) -> String {
+    println!("{}", s);
+    println!("{}", s.len());
+    s
+}
+
+fn val_and_borrowing(s: &String) -> () {
+    println!("{}", s);
+    println!("{}", s.len());
+}
+
+fn var_and_ownership_not_returned(mut ms: String) -> () {
+    ms.push_str("_I'm_mutable");
+    println!("{}", ms);
+}
+
+fn var_and_ownership_returned(mut ms: String) -> String {
+    ms.push_str("_I'm_mutable  - too!");
+    println!("{}", ms);
+    println!("{}", ms.len());
+    ms
+}
+
+fn var_and_borrowing(mut ms: &mut String) -> () {
+    ms.push_str("_I'm_mutable_and borrowed...");
+    println!("{}", ms);
+    println!("{}", ms.len());
+}
+
 fn main() {
-    let hello = String::new();
-    println!("{}", hello); // prints nothing
+    let hello = String::from("hello there!");
+    val_and_ownership_not_returned(hello);
 
-    let hello = 42;
-    println!("{}", hello);
+    let dogs = String::from("my dogs");
+    let dogs2 = val_and_ownership_returned(dogs);
+    println!("{}", dogs2);
 
-    let hello = String::from("hello");
-    println!("{}", &hello);
+    let horses = String::from("my horses");
+    val_and_borrowing(&horses);
+    println!("{}", horses);
 
-    let mut hello_2 = String::from("hello_2");
-    println!("{}", hello_2);
+    let fish = String::from("my fish");
+    var_and_ownership_not_returned(fish);
 
-    hello_2.push('2');
-    hello_2.push_str("_skidoo!");
-    println!("{}", hello_2);
+    let goats = String::from("my goats");
+    let goats2 = var_and_ownership_returned(goats);
+    println!("{}", goats2);
 
-    let hello_22 = &mut hello_2;
-    // println!("{}", hello_2); // cannot borrow `hello_2` as immutable
-    // because it is also borrowed as mutable
-    println!("{}", hello_22);
+    let mut worms = String::from("my worms");
+    var_and_borrowing(&mut worms);
+    println!("{}", worms);
 
-    hello_22.push_str("_last_one");
-    println!("{}", hello_22);
+    // these are all still valid:
+    println!("{}", dogs2);
+    println!("{}", horses);
+    println!("{}", goats2);
+    println!("{}", worms);
+
+    // while these are gone (moved):
+    // println!("{}", hello);
+    // println!("{}", fish);
 }
