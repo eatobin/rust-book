a961ac0f172c17bed502abc3fb09045daa710db6
Eric Tobin <eatobin@gmail.com>
2018-12-12 19:36:31 -0600

    progress

--- eat_structs/src/main.rs
+++ eat_structs/src/main.rs
@@ -21,12 +21,14 @@ struct Nil;
 struct Pair(i32, f32);
 
 // A struct with two fields
+#[derive(Debug)]
 struct Point {
     x: f32,
     y: f32,
 }
 
 // Structs can be reused as fields of another struct
+#[derive(Debug)]
 #[allow(dead_code)]
 struct Rectangle {
     p1: Point,
@@ -35,9 +37,9 @@ struct Rectangle {
 
 fn main() {
     // Create struct with field init shorthand
-    let name = "Peter";
-    let age = 27;
-    let mut peter = Person { name, age };
+    let name: &str = "Peter";
+    let age: u8 = 27;
+    let mut peter: Person = Person { name, age };
 
     // Print debug struct
     println!("{:?}", peter);
@@ -48,7 +50,13 @@ fn main() {
     peter.set_name("Tony");
     peter.greet();
 
-     println!("{:?}", Person {name: "Doofy", age: 22});
+    println!(
+        "{:?}",
+        Person {
+            name: "Doofy",
+            age: 22
+        }
+    );
 
     // Instantiate a `Point`
     let point: Point = Point { x: 0.3, y: 0.4 };
@@ -57,30 +65,32 @@ fn main() {
     println!("point coordinates: ({}, {})", point.x, point.y);
 
     // Make a new point by using struct update syntax to use the fields of our other one
-    let new_point = Point { x: 0.1, ..point };
+    let new_point: Point = Point { x: 0.1, ..point };
     // `new_point.y` will be the same as `point.y` because we used that field from `point`
     println!("second point: ({}, {})", new_point.x, new_point.y);
 
     // Destructure the point using a `let` binding
-    let Point { x: my_x, y: my_y } = point;
+    let Point { x: my_x, y: my_y }: Point = point;
 
-    let _rectangle = Rectangle {
+    let _rectangle: Rectangle = Rectangle {
         // struct instantiation is an expression too
         p1: Point { x: my_y, y: my_x },
         p2: point,
     };
 
+    println!("{:?}", _rectangle);
+
     // Instantiate a unit struct
-    let _nil = Nil;
+    let _nil: Nil = Nil;
 
     // Instantiate a tuple struct
-    let pair = Pair(1, 0.1);
+    let pair: Pair = Pair(1, 0.1);
 
     // Access the fields of a tuple struct
     println!("pair contains {:?} and {:?}", pair.0, pair.1);
 
     // Destructure a tuple struct
-    let Pair(integer, decimal) = pair;
+    let Pair(integer, decimal): Pair = pair;
 
     println!("pair contains {:?} and {:?}", integer, decimal);
 }
