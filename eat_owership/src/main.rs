e736e3eadd4148a3671579cb9100bc1f92eda05c
Eric Tobin <eatobin@gmail.com>
2019-01-21 15:21:18 -0600

    found strings

--- eat_owership/src/main.rs
+++ eat_owership/src/main.rs
@@ -205,6 +205,52 @@ fn main() {
 
     assert_eq!(total_bytes, 16);
     use_names_for_something_else(names);
+
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    // `iter()` for vecs yields `&i32`.
+    let mut iter = vec1.iter();
+    // `into_iter()` for vecs yields `i32`.
+    let mut into_iter = vec2.into_iter();
+
+    // A reference to what is yielded is `&&i32`. Destructure to `i32`.
+    println!("Find 2 in vec1: {:?}", iter     .find(|&&x| x == 2));
+    // A reference to what is yielded is `&i32`. Destructure to `i32`.
+    println!("Find 2 in vec2: {:?}", into_iter.find(| &x| x == 2));
+
+    let vec1 = vec!["one", "two", "three"];
+    let vec2 = vec!["four", "five", "six"];
+
+    let mut iter = vec1.iter();
+    let mut into_iter = vec2.into_iter();
+
+    // A reference to what is yielded is `&&i32`. Destructure to `i32`.
+    println!("Find 2 in vec1: {:?}", iter     .find(|&&x| x == "two"));
+    // A reference to what is yielded is `&i32`. Destructure to `i32`.
+    println!("Find 2 in vec2: {:?}", into_iter.find(| &x| x == "two"));
+
+    let vec1 = vec![&1, &2, &3];
+    let vec2 = vec![&4, &5, &6];
+
+    let mut iter = vec1.iter();
+    let mut into_iter = vec2.into_iter();
+
+    // A reference to what is yielded is `&&i32`. Destructure to `i32`.
+    println!("Find 2 in vec1: {:?}", iter     .find(|&&x| x == &1));
+    // A reference to what is yielded is `&i32`. Destructure to `i32`.
+    println!("Find 2 in vec2: {:?}", into_iter.find(| &x| x == &2));
+
+    let vec1 = vec!["one".to_owned(), "two".to_owned(), "three".to_owned()];
+    let vec2 = vec!["four".to_owned(), "five".to_owned(), "six".to_owned()];
+
+    let mut iter = vec1.iter();
+    let mut into_iter = vec2.into_iter();
+
+    // A reference to what is yielded is `&&i32`. Destructure to `i32`.
+    println!("Find 2 in vec1: {:?}", iter     .find(| x| x == &&"three".to_owned()));
+    // A reference to what is yielded is `&i32`. Destructure to `i32`.
+    println!("Find 2 in vec2: {:?}", into_iter.find(| x| x == &"six".to_owned()));
 }
 
 fn use_names_for_something_else(_names: Vec<&str>) {
