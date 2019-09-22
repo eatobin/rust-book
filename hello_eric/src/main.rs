e2761a8df928afc4e03c702c13413476f52ef855
Eric Tobin <eatobin@gmail.com>
2019-09-22 09:40:44 -0500

    friends

--- hello_eric/src/main.rs
+++ hello_eric/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, Eric!");
+    let friends = vec!["me".to_owned(), "you".to_owned()];
+
+    for mut f in friends {
+        f.push('?');
+        println!("{}", f);
+    }
 }
