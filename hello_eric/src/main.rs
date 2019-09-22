9df366820315c0322f569082e0b3e911c9c698c7
Eric Tobin <eatobin@gmail.com>
2019-09-22 09:52:53 -0500

    json

--- hello_eric/src/main.rs
+++ hello_eric/src/main.rs
@@ -1,8 +1,19 @@
-fn main() {
-    let friends = vec!["me".to_owned(), "you".to_owned()];
+extern crate rustc_serialize;
 
-    for mut f in friends {
-        f.push('?');
-        println!("{}", f);
-    }
+use rustc_serialize::json;
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Penguin {
+    name: String,
+    born: i16,
+}
+fn main() {
+    let pengu = Penguin {
+        name: "pengu".to_string(),
+        born: 1999,
+    };
+    println!("{}", json::encode(&pengu).unwrap());
+    let pingu: Penguin = json::decode(r##"{"name":"pingu","born":2001}"##).unwrap();
+    assert_eq!(&pingu.name, "pingu");
+    assert_eq!(pingu.born, 2001);
 }
